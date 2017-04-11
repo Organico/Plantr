127cfd36dfa70e450aba2d081ccae030f0e91693
Ryan Perry <Rperry2174>
2017-04-11 18:53:51 -0400

    Post request working for makeGardenSquareGrid and dynamically rendering when getting a grid back

--- db/models/Plant.js
+++ db/models/Plant.js
@@ -1,21 +1,35 @@
 var mongoose = require('mongoose');
 
 var plantSchema = new mongoose.Schema({
-  plantId: {type: String, unique: true},
-  name: String,
-  description: String,
-  season: String,
-  instruction: String,
-  hardinessZone: String,
-  price: Number,
-  quantity: String,
-  sunlight: String,
-  germination: String,
-  harvest: Number,
-  rowSpacing: Number,
-  extremeWarning: Array
+  plantId: String,
+  color: String
 });
 
 var Plant = mongoose.model('Plant', plantSchema);
 
 module.exports = Plant;
+
+  // description: String,
+  // season: String,
+  // instruction: String,
+  // hardinessZone: String,
+  // price: Number,
+  // quantity: String,
+  // sunlight: String,
+  // germination: String,
+  // harvest: Number,
+  // rowSpacing: Number,
+  // extremeWarning: Array
+
+      //   "name": "sunflower",
+      // "description": "A massive beast of a flower",
+      // "season": "summer" ,
+      // "instructions": "Cover and keep watered until seeds sprout in 7 to 10 days. When first true leaves appear (the second set of leaves); thin plants to about 2 feet apart. Depending on the variety, sunflowers will mature and develop seeds in 80 to 120 days. Sow a new row every 2 to 3 weeks to enjoy continuous blooms until the first frost.",
+      // "hardinessZone": ">5",
+      // "price": 5,
+      // "quantity": "1 packet",
+      // "sunlight": "full-sun",
+      // "germination": "10-14 days",
+      // "harvest": 55,
+      // "rowSpacing": 1.5, //TBD
+      // "extremeWarning": [60, 90]   //insert ideal range, determine F or C
--- src/GardenSquareGrid/makeGardenSquareGrid.js
+++ src/GardenSquareGrid/makeGardenSquareGrid.js
@@ -9,9 +9,35 @@ import {Layer, Rect, Circle, Stage, Group} from 'react-konva';
 import PlantShelf from './PlantShelf.js'
 
 import PlantGrid from './PlantGrid.js';
+import querystring from 'querystring'
 
 
 const MakeGardenSquareGridView = React.createClass({
+
+ // querystring.stringify({
+ //        gardenId: 10,
+ //        userId: 10,
+ //        gardenGrid: this.props.gardenGrid,
+ //        plantGrid: this.props.plantGrid
+ //     })
+
+
+  saveGarden() {
+    console.log("Saving garden...")
+    axios.post('/api/gardens',
+      {
+        gardenId: Math.random()*100,
+        userId: Math.random()*100,
+        gardenGrid: this.props.gardenGrid,
+        plantGrid: this.props.plantGrid
+      }
+    ).then((res) => {
+      console.log("Successful post");
+    }).catch((err) => {
+      console.error(err);
+      console.log("Error in getGardenSquareGrid getAllGardens()")
+    });
+  },
 
   render () {
     let input;
@@ -27,6 +53,10 @@ const MakeGardenSquareGridView = React.createClass({
               <button className="btn btn-primary btn-sm" onClick={() => {
                 this.props.dispatchSetGardenParameters(+width.value, +height.value, "green");
             }} type="submit">Set Garden Parameters
+            </button>
+             <button className="btn btn-primary btn-sm" onClick={() => {
+                  this.saveGarden();
+                       }} type="submit">Submit Garden
             </button>
 
           <div >
@@ -46,8 +76,8 @@ const MakeGardenSquareGridView = React.createClass({
 
 const mapStateToProps = (state) => {
   return {
-    searchTerm: state.searchTerm,
-
+    gardenGrid: state.gardenGrid,
+    plantGrid: state.plantGrid
   };
 };
 
