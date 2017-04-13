3e4c4f980027ddaab0a2ce1fd564f30025f8da43
Ariel <Ariel.Salem1989@gmail.com>
2017-04-12 20:03:46 -0700

    Git added home

---
+++ src/Home.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { setGardenParameters, setGarden } from './Actions/GardenActions';
+import axios from 'axios';
+
+const Home = React.createClass({
+
+  render () {
+    let input;
+    let width;
+    let height;
+    let color;
+
+    return (
+        <div className="text-center">
+          <input ref={(node) => width = node } type="number" name="width" placeholder='Feet [width] is your garden?'/>
+          <input ref={(node) => height = node } type="number" name="height" placeholder='Feet [height] is your garden?'/>
+          <button className="btn btn-primary btn-sm" onClick={() => {
+            axios.get('/api/gardens').then((res) => {
+              var dbGardenGrid = res.data[0].gardenGrid;
+              this.props.dispatchSetGarden(dbGardenGrid);
+            }).catch((err) => {
+              console.error(err);
+            });
+          }} type="submit">makeGrid
+          </button>
+        </div>
+    );
+  }
+});
+
+const mapStateToProps = (state) => {
+  return {
+    searchTerm: state.searchTerm
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+
+    dispatchSetGardenParameters(width, height, color) {
+      dispatch(setGardenParameters(width, height, color));
+    },
+    dispatchSetGarden(dbGardenGrid) {
+      dispatch(setGarden(dbGardenGrid));
+    }
+  };
+};
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
