1373496d17822da3624ad2ed0a0945a0de0b02fa
Ryan Perry <Rperry2174>
2017-04-13 14:28:31 -0400

    Updated color scheme

--- src/Home/NewsFeed.js
+++ src/Home/NewsFeed.js
@@ -1,7 +1,11 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux'
 
-const NewsFeed = (props) => {
+// const NewsFeed = (props) => {
+
+class NewsFeed extends React.Component {
+
+  render(){
   let pos = {
     marginLeft: '1000px'
   }
@@ -10,28 +14,29 @@ const NewsFeed = (props) => {
     height: '300px'
   }
 
-  return (
-      <div className="card col-xs-5" style={pos}>
-          <div className="row">
-            <img className="card-img" style={maxHeight} src='https://static.pexels.com/photos/296230/pexels-photo-296230.jpeg' alt="Card image" />
-            <div className="card-img-overlay">
-              <p className="card-text">I'm text that has a background image!</p>
+    return (
+        <div className="card col-xs-5" style={pos}>
+            <div className="row">
+              <img className="card-img" style={maxHeight} src='https://static.pexels.com/photos/296230/pexels-photo-296230.jpeg' alt="Card image" />
+              <div className="card-img-overlay">
+                <p className="card-text">I'm text that has a background image!</p>
+              </div>
             </div>
-          </div>
-          <div className="row">
-            <img className="card-img" style={maxHeight} src='https://static.pexels.com/photos/169523/pexels-photo-169523.jpeg' alt="Card image" />
-            <div className="card-img-overlay">
-              <p className="card-text">I'm text that has a background image!</p>
+            <div className="row">
+              <img className="card-img" style={maxHeight} src='https://static.pexels.com/photos/169523/pexels-photo-169523.jpeg' alt="Card image" />
+              <div className="card-img-overlay">
+                <p className="card-text">I'm text that has a background image!</p>
+              </div>
             </div>
-          </div>
-          <div className="row">
-            <img className="card-img" style={maxHeight} src='https://static.pexels.com/photos/136183/pexels-photo-136183.jpeg' alt="Card image" />
-            <div className="card-img-overlay">
-              <p className="card-text">I'm text that has a background image!</p>
+            <div className="row">
+              <img className="card-img" style={maxHeight} src='https://static.pexels.com/photos/136183/pexels-photo-136183.jpeg' alt="Card image" />
+              <div className="card-img-overlay">
+                <p className="card-text">I'm text that has a background image!</p>
+            </div>
           </div>
         </div>
-      </div>
-    )
+      )
+    }
   }
 
 export default NewsFeed;
