f77e02eaa678a5c7648e0ae87eb5cc4ec24b0e83
Nathan Brewer-Davis <10ndavis10@gmail.com>
2017-04-19 21:15:31 -0500

    THEME OVERHALL
    THEME OVERHALL

--- src/userProfile/About.js
+++ src/userProfile/About.js
@@ -4,10 +4,12 @@ import React, { Component } from 'react';
 class About extends Component {
   render() {
     return (
-      <div className="col-md-3 offset-md-0 col-sm-6 offset-sm-0 col-xs-12 right userRecent">
-        <div className="userRecentSpan"> About Me </div>
-        <div>
-          <p>This is a paragraph about myself. I'm so cool. Look at all this cool stuff about me!</p>
+      <div className="row">
+        <div className="col-md-6 offset-md-3 right userRecent">
+          <div className="userRecentSpan"> About Me </div>
+          <div>
+            <p>This is a paragraph about myself. I'm so cool. Look at all this cool stuff about me!</p>
+          </div>
         </div>
       </div>
       )
