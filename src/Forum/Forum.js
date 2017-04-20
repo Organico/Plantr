bc41fb75eb3add5517f6b916ccce1e5092462557
Ariel <Ariel.Salem1989@gmail.com>
2017-04-19 19:16:58 -0700

    embedded comments, added delete button per user

--- src/Forum/Forum.js
+++ src/Forum/Forum.js
@@ -5,7 +5,7 @@ import ForumPost from './ForumPost';
 import CreateNewPost from './CreateNewPost';
 import axios from 'axios';
 import { setPosts } from '../Actions/ForumActions';
-
+import auth from '../client.js';
 
 const Forum = React.createClass({
 
@@ -21,11 +21,26 @@ const Forum = React.createClass({
       this.props.dispatchSetPost(dbPostData)
     }).catch((err) => {
       console.error(err);
-      console.log("Error in Forum()");
+    });
+  },
+
+   deletePost() {
+    axios.delete('/api/forum/')
+    .then((res) => {
+      let dbPostData = res.data;
+      for (let i = 0; i<dbPostData.length; i++) {
+        let message = dbPostData[i];
+        message['isShort'] = true;
+      }
+      console.log("Db post data", dbPostData)
+      this.props.dispatchSetPost(dbPostData)
+    }).catch((err) => {
+      console.error(err);
     });
   },
 
   render() {
+    const profile = auth.getProfile();
     return(
         <div className="row">
           <button type="submit" onClick={ () => {
@@ -35,8 +50,18 @@ const Forum = React.createClass({
             <CreateNewPost />
           </div>
           <div className="col-md-6">
-            {this.props.posts.map((post, i) =>
-                <ForumPost key={i} post={post} nickname={post.nickname} title={post.title} message={post.message} replies={post.replies} />
+            {this.props.posts.map((post, i) => {
+              if (profile.email === post.email) {
+               return <div>
+               <ForumPost key={i} post={post} nickname={post.nickname} title={post.title} message={post.message} replies={post.replies} />
+                <button type="submit" onClick={ () => {
+                  this.deletePost();
+                }}>delete</button>
+               </div>
+              } else {
+               return <ForumPost key={i} post={post} nickname={post.nickname} title={post.title} message={post.message} replies={post.replies} />
+              }
+            }
             )}
           </div>
         </div>
