24a388cf0360157ec7383d7596ba1a64d0974425
Ryan Perry <Rperry2174>
2017-04-21 15:43:49 -0400

    added path to S3 bucket for tomatoes. Also Made MaterialGrid use the non-deprecated React declaration

--- src/SimpleSpin/MaterialGrid.js
+++ src/SimpleSpin/MaterialGrid.js
@@ -2,108 +2,74 @@ import * as THREE from 'three';
 
 import React, { Component, PropTypes } from 'react';
 import MaterialCube from './MaterialCube';
+import { connect } from 'react-redux';
+
 import store from '../store';
 
 console.log("Store in MaterialGrid.js", store.getState())
 console.log("store {} ", {store})
 
-const MaterialGrid = React.createClass({
+class MaterialGrid extends React.Component {
+
+    constructor(props, context) {
+      super(props,context)
+      this.gardenGrid = store.getState().gardenReducer.gardenGrid
+    }
+
+
     render() {
-      // const {
-      //   store
-      // } = this.props
-      const gardenGrid = store.getState().gardenReducer.gardenGrid
-      console.log("here is the the this.props in MAterial Greid: ", gardenGrid)
       return (
         <group>
-          {gardenGrid.map((materialCube, i) =>
+          {this.gardenGrid.map((materialCube, i) =>
             <MaterialCube key={i}
               width={100}
-              height={100}
+              height={20}
               depth={100}
               position={new THREE.Vector3(
-                materialCube.x - 500,
+                materialCube.x * 2 - 500,
                 0,
-                materialCube.y - 500
+                materialCube.y * 2 - 500
               )}
-              map={"https://s3-us-west-2.amazonaws.com/ryaperry-bucket/grasslight-big.jpg"}
+              map={materialCube.img}
             />
           )}
         </group>
       )
     }
-})
+}
 
-// const mapStateToProps = (state) => {
-//   return {
-//     gardenGrid: state.gardenReducer.gardenGrid
-//   }
-// }
-
-// const mapDispatchToProps = (dispatch) => {
-//   return {
-//     // dispatchMovePiece (location) {
-//     //   dispatch(movePiece(location))
-//     // }
-//   }
-// };
-
-// export default connect(mapStateToProps, mapDispatchToProps)(MaterialGrid)
-export default MaterialGrid
+export default(MaterialGrid)
 
 
-
-
-
-
-
-// class MaterialGrid extends React.Component {
-
-//     constructor(props, context) {
-//       super(props, context);
-//       this.gardenGrid = store.getState().gardenReducer.gardenGrid
-
-//     }
-
-
+// const MaterialGrid = React.createClass({
 //     render() {
+//       // const {
+//       //   store
+//       // } = this.props
+//       const gardenGrid = store.getState().gardenReducer.gardenGrid
+//       console.log("here is the the this.props in MAterial Greid: ", gardenGrid)
 //       return (
 //         <group>
-//           {this.gardenGrid.map((materialCube, i) =>
+//           {gardenGrid.map((materialCube, i) =>
 //             <MaterialCube key={i}
 //               width={100}
-//               height={100}
+//               height={20}
 //               depth={100}
 //               position={new THREE.Vector3(
-//                 materialCube.x,
+//                 materialCube.x * 2 - 500,
 //                 0,
-//                 materialCube.y
+//                 materialCube.y * 2 - 500
 //               )}
-//               map={"https://s3-us-west-2.amazonaws.com/ryaperry-bucket/grasslight-big.jpg"}
+//               map={materialCube.img}
 //             />
 //           )}
 //         </group>
 //       )
 //     }
-// }
+// })
 
-// // const mapStateToProps = (state) => {
-// //   return {
-// //     gardenGrid: state.gardenReducer.gardenGrid
-// //   }
-// // }
+// export default MaterialGrid
 
-// // const mapDispatchToProps = (dispatch) => {
-// //   return {
-// //     // dispatchMovePiece (location) {
-// //     //   dispatch(movePiece(location))
-// //     // }
-// //   }
-// // };
-
-// // export default connect(mapStateToProps, mapDispatchToProps)(MaterialGrid)
-
-// export default(MaterialGrid)
 
 
 
