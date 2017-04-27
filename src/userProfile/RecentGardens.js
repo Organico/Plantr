43ebaaff2fa174670de9b526b96f2b6e8d4beebb
Ariel <Ariel.Salem1989@gmail.com>
2017-04-27 16:07:23 -0700

    thanks for the help samy

--- src/userProfile/RecentGardens.js
+++ src/userProfile/RecentGardens.js
@@ -3,6 +3,7 @@ import GardenSquareGridView from '../GardenSquareGrid/getGardenSquareGrid';
 import ReactDOM from 'react-dom'
 import { connect } from 'react-redux';
 import IndividualGarden from './IndividualGarden';
+import IndividualGardenInfo from './IndividualGardenInfo';
 import axios from 'axios';
 import auth from '../client.js';
 import { setPosts } from '../Actions/ForumActions';
@@ -19,13 +20,14 @@ class RecentGardens extends Component {
 
   toggleDropDown() {
     console.log('toggleDropDown is being toggled');
-    this.setState({this.state.dropDown: !this.state.dropDown});
+    this.setState({dropDown: !this.state.dropDown});
   }
 
   getUserGardens() {
     const profile = auth.getProfile();
     axios.get('/api/gardens/' + profile.email).then((res) => {
       let personalGarden = res.data;
+      userGardens = [];
       for (let i = 0; i < personalGarden.length; i++) {
         let gardenObj = {
           gardenGrid: personalGarden[i].gardenGrid,
@@ -35,7 +37,6 @@ class RecentGardens extends Component {
           profilePicture: personalGarden[i].profilePicture
         }
         userGardens.push(gardenObj);
-        console.log('userGardens: ', userGardens)
       }
     }).catch((err) => {
       console.error("There was a get request error on the client in User RecentGardens", err);
@@ -48,17 +49,16 @@ class RecentGardens extends Component {
 
   render() {
     const profile = auth.getProfile();
-    console.log('PROFILE: ', profile)
     return (
       <div className="row">
         <div className="col-md-12 offset-md-2 right userGarden">
           <div className="userGardenSpan">
             <h3>Recent Gardens</h3>
               { userGardens.map((garden, i) => {
+
                 if (!this.state.dropDown) {
-                  console.log('THIS IS THE THIS: ', this);
                   return <div>
-                    <IndividualGarden gardenName={garden.gardenName} nickname={profile.nickname} profilePicture={garden.profilePicture} onClick={ () => { this.toggleDropDown(); }}/>
+                    <IndividualGardenInfo gardenName={garden.gardenName} nickname={profile.nickname} profilePicture={garden.profilePicture} onClick={ () => { this.toggleDropDown(); }}/>
                     </div>
                   } else {
                     return <IndividualGarden gardenName={garden.gardenName} plantGrid={garden.plantGrid} gardenGrid={garden.gardenGrid} nickname={profile.nickname} profilePicture={garden.profilePicture} onClick={ () => { this.toggleDropDown(); }}/>
