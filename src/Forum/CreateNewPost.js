f6ebf78db7592f070025ae9d6f5014d4a8ac18d7
Ariel <Ariel.Salem1989@gmail.com>
2017-04-26 19:10:10 -0700

    fixed functionality

--- src/Forum/CreateNewPost.js
+++ src/Forum/CreateNewPost.js
@@ -47,6 +47,7 @@ class CreateNewPost extends Component {
         this.props.closeModal();
         this.props.dispatchSetEditing();
         this.getPost();
+        this.props.dispatchSetEditing();
       }).catch((err) => {
         console.error("Error in creating a new post on CreateNewPost", err);
       });
