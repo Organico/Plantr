5f55d145816324e88ffd01e37377cdc3bf2078dd
Ariel <Ariel.Salem1989@gmail.com>
2017-07-08 16:22:38 -0700

    modifications to aboutJS layout - not functional

--- src/userProfile/About.js
+++ src/userProfile/About.js
@@ -1,31 +1,16 @@
-import React, { Component } from 'react';
-import axios from 'axios';
 import auth from '../client.js';
+import axios from 'axios';
+import React, { Component } from 'react';
 
 let userAboutObject = {};
 userAboutObject.about = "Farming is a big passion of mine that I inherited from my father. He was raised on a mango and banana farm in the outskirts of Ismalia, Egypt. My current passion is trying to grow hardy plants in unexpected places, including Russian Pomegranates and Hardy Kiwis in Northern Virginia."
+
 class About extends Component {
   constructor() {
     super()
     this.state = {
       edit: false
     }
-  }
-
-  toggleState() {
-    this.setState({edit: !this.state.edit});
-  }
-
-  setAbout(id, about) {
-    axios.put('api/users/' + id + '/', {
-      about: about
-    }).then((res) => {
-      console.log("Successfully posted your AboutMe! ", res.data);
-      this.toggleState();
-      this.getAbout();
-    }).catch((err) => {
-      console.error("Error in submitting your AboutMe - About.js: ", err);
-    });
   }
 
   getAbout() {
@@ -42,40 +27,59 @@ class About extends Component {
     });
   }
 
+  renderAboutMe() {
+    let newMessage;
+    // functionality doesn't exist - needs to be reworked
+    if (this.state.edit) {
+      return (
+        <div>
+          <textarea className="textArea" rows="4" ref={(message) => newMessage = message } type="string" name="newMessage" defaultValue={JSON.parse(userAboutObject.about)}>
+          </textarea>
+          <button type="submit" onClick ={ () => {
+            newMessage.value = JSON.stringify(newMessage.value);
+            this.setAbout(userAboutObject.id, newMessage.value);
+            newMessage.value = '';
+          }}>submit</button>
+        </div>
+      )
+    }
+    return (
+      <div>
+        <p>{userAboutObject.about}</p>
+        <button type="submit" onClick ={ () => {
+          this.toggleState();
+          }}>edit</button>
+      </div>
+      )
+   }
+
+  setAbout(id, about) {
+    axios.put('api/users/' + id + '/', {
+      about: about
+    }).then((res) => {
+      console.log("Successfully posted your AboutMe! ", res.data);
+      this.toggleState();
+      this.getAbout();
+    }).catch((err) => {
+      console.error("Error in submitting your AboutMe - About.js: ", err);
+    });
+  }
+
+  toggleState() {
+    this.setState({edit: !this.state.edit});
+  }
+
   componentDidMount() {
     // this.getAbout();
   }
 
   render() {
-    const profile = auth.getProfile();
-    let that = this;
-    let newMessage;
     return (
       <div className="row userAboutMe">
         <div className="col-md-12 about">
           <h3 id="aboutMe"> About Me </h3>
           <div>
-          { (function() {
-            if (that.state.edit) {
-              return <div>
-                <textarea className="textArea" rows="4" ref={(message) => newMessage = message } type="string" name="newMessage" defaultValue={JSON.parse(userAboutObject.about)}>
-                </textarea>
-                <button type="submit" onClick ={ () => {
-                  newMessage.value = JSON.stringify(newMessage.value);
-                  that.setAbout(userAboutObject.id, newMessage.value);
-                  newMessage.value = '';
-                }}>submit</button>
-            </div>
-            } else{
-              return <div>
-                <p>{userAboutObject.about}</p>
-                <button type="submit" onClick ={ () => {
-                  that.toggleState();
-              }}>edit</button>
-            </div>
-            }
-          }())
-          }
+            { this.renderAboutMe() }
           </div>
         </div>
       </div>
