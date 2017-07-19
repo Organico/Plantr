746dce06709ddc693d5cc29aa7cb0f7e472d7e85
Ariel <Ariel.Salem1989@gmail.com>
2017-07-18 18:34:23 -0700

    added styling for about us page

--- src/About/AboutUs.js
+++ src/About/AboutUs.js
@@ -3,47 +3,82 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom'
 
 class AboutUs extends Component {
-  constructor() {
-    super()
-  }
   render() {
 
     return (
         <div className="row topAbout">
           <div className="col-md-6">
-            <a target="_blank" href="https://www.linkedin.com/in/ariel-salem-43199412a">
+            <div className="row">
+              <a target="_blank" href="https://www.linkedin.com/in/ariel-salem-43199412a">
               <div className="profiles">
                 <p className="name">Ariel Salem</p>
                 <img src="https://media.licdn.com/mpr/mpr/shrink_100_100/AAEAAQAAAAAAAAh_AAAAJGY5YTM4MzJkLTcxNWMtNDU2Yy04NjNlLTZkODFkMzZkNzYwZA.jpg" />
+                <br />
+                <br />
+                <br />
+                <br />
+                <div className="row profile-description">
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                  </p>
+                </div>
               </div>
             </a>
           </div>
-          <div className="col-md-6">
+        </div>
+        <div className="col-md-6">
+          <div className="row">
             <a target="_blank" href="https://www.linkedin.com/in/nathan-brewer-davis-604a36112">
-              <div className="profiles">
-                <p className="name">Nathan Brewer-Davis</p>
-                <img src="https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAmHAAAAJGVjNmU3MmJmLWI4ZTItNDFiMC05OWZjLWVlODI5NWY0Y2QwYg.jpg" />
-              </div>
+             <div className="profiles">
+               <p className="name">Nathan Brewer-Davis</p>
+               <img src="https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAmHAAAAJGVjNmU3MmJmLWI4ZTItNDFiMC05OWZjLWVlODI5NWY0Y2QwYg.jpg" />
+               <br />
+               <br />
+               <br />
+               <br />
+               <div className="row profile-description">
+                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                 </p>
+               </div>
+             </div>
             </a>
           </div>
-          <div className="col-md-6">
+        </div>
+        <div className="col-md-6">
+          <div className="row">
             <a target="_blank" href="https://www.linkedin.com/in/ryanaperry/">
-              <div className="profiles">
-                <p className="name">Ryan Perry</p>
-                <img src="https://media.licdn.com/mpr/mpr/shrinknp_200_200/p/6/005/05c/3b3/0de3dc3.jpg" />
-              </div>
+            <div className="profiles">
+              <p className="name">Ryan Perry</p>
+              <img src="https://media.licdn.com/mpr/mpr/shrinknp_200_200/p/6/005/05c/3b3/0de3dc3.jpg" />
+               <br />
+               <br />
+               <br />
+               <br />
+               <div className="row profile-description">
+                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                 </p>
+               </div>
+             </div>
             </a>
           </div>
-
-          <div className="col-md-6">
+        </div>
+        <div className="col-md-6">
+          <div className="row">
             <a target="_blank" href="https://www.linkedin.com/in/samykebaish">
               <div className="profiles">
                 <p className="name">Samy Kebaish</p>
                 <img src="https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAQUAAAAJDExZTYyY2YzLTNmNTUtNGJkZi05ZTkzLTQ1OTk5ZDM2ZTY1Mw.jpg" />
               </div>
+               <br />
+               <br />
+               <br />
+               <br />
+               <div className="row profile-description">
+                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                 </p>
+               </div>
             </a>
           </div>
-
+        </div>
         </div>
     )
   }
